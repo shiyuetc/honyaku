bdc44dcaae20eef5abc09934119fd3e4d4d4f518
shiyuetc <shiyuetc@users.noreply.github.com>
2020-04-20 22:45:21 +0900

    Implement

--- honyaku/MainForm.Designer.cs
+++ honyaku/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace honyaku
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// 必要なデザイナー変数です。
@@ -28,13 +28,239 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.BackToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TranslateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ReTranslateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.表示VToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.翻訳コンテナToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LateralSplitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.VerticalitySplitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ClientPanel = new System.Windows.Forms.Panel();
+            this.TranslatorSplitContainer = new System.Windows.Forms.SplitContainer();
+            this.SourceLanguageLabel = new System.Windows.Forms.Label();
+            this.SourceTextBox = new System.Windows.Forms.TextBox();
+            this.TargetLanguageLabel = new System.Windows.Forms.Label();
+            this.TargetTextBox = new System.Windows.Forms.TextBox();
+            this.StealthModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TranslatorSplitContainer)).BeginInit();
+            this.TranslatorSplitContainer.Panel1.SuspendLayout();
+            this.TranslatorSplitContainer.Panel2.SuspendLayout();
+            this.TranslatorSplitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.BackToolStripMenuItem,
+            this.TranslateToolStripMenuItem,
+            this.ReTranslateToolStripMenuItem,
+            this.表示VToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(502, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.MouseEnter += new System.EventHandler(this.menuStrip1_MouseEnter);
+            this.menuStrip1.MouseLeave += new System.EventHandler(this.menuStrip1_MouseLeave);
+            // 
+            // BackToolStripMenuItem
+            // 
+            this.BackToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("BackToolStripMenuItem.Image")));
+            this.BackToolStripMenuItem.Name = "BackToolStripMenuItem";
+            this.BackToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.BackToolStripMenuItem.Text = "戻る";
+            this.BackToolStripMenuItem.Visible = false;
+            this.BackToolStripMenuItem.Click += new System.EventHandler(this.BackToolStripMenuItem_Click);
+            // 
+            // TranslateToolStripMenuItem
+            // 
+            this.TranslateToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("TranslateToolStripMenuItem.Image")));
+            this.TranslateToolStripMenuItem.Name = "TranslateToolStripMenuItem";
+            this.TranslateToolStripMenuItem.Size = new System.Drawing.Size(110, 20);
+            this.TranslateToolStripMenuItem.Text = "キャプチャ+翻訳";
+            this.TranslateToolStripMenuItem.Click += new System.EventHandler(this.TranslateToolStripMenuItem_Click);
+            // 
+            // ReTranslateToolStripMenuItem
+            // 
+            this.ReTranslateToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("ReTranslateToolStripMenuItem.Image")));
+            this.ReTranslateToolStripMenuItem.Name = "ReTranslateToolStripMenuItem";
+            this.ReTranslateToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.ReTranslateToolStripMenuItem.Text = "再翻訳";
+            this.ReTranslateToolStripMenuItem.Visible = false;
+            this.ReTranslateToolStripMenuItem.Click += new System.EventHandler(this.ReTranslateToolStripMenuItem_Click);
+            // 
+            // 表示VToolStripMenuItem
+            // 
+            this.表示VToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StealthModeToolStripMenuItem,
+            this.翻訳コンテナToolStripMenuItem});
+            this.表示VToolStripMenuItem.Name = "表示VToolStripMenuItem";
+            this.表示VToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.表示VToolStripMenuItem.Text = "表示(&V)";
+            // 
+            // 翻訳コンテナToolStripMenuItem
+            // 
+            this.翻訳コンテナToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.LateralSplitToolStripMenuItem,
+            this.VerticalitySplitToolStripMenuItem});
+            this.翻訳コンテナToolStripMenuItem.Name = "翻訳コンテナToolStripMenuItem";
+            this.翻訳コンテナToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.翻訳コンテナToolStripMenuItem.Text = "翻訳コンテナ";
+            // 
+            // LateralSplitToolStripMenuItem
+            // 
+            this.LateralSplitToolStripMenuItem.Checked = true;
+            this.LateralSplitToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.LateralSplitToolStripMenuItem.Name = "LateralSplitToolStripMenuItem";
+            this.LateralSplitToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.LateralSplitToolStripMenuItem.Text = "左右に分割";
+            this.LateralSplitToolStripMenuItem.Click += new System.EventHandler(this.LateralSplitToolStripMenuItem_Click);
+            // 
+            // VerticalitySplitToolStripMenuItem
+            // 
+            this.VerticalitySplitToolStripMenuItem.Name = "VerticalitySplitToolStripMenuItem";
+            this.VerticalitySplitToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.VerticalitySplitToolStripMenuItem.Text = "上下に分割";
+            this.VerticalitySplitToolStripMenuItem.Click += new System.EventHandler(this.VerticalitySplitToolStripMenuItem_Click);
+            // 
+            // ClientPanel
+            // 
+            this.ClientPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ClientPanel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientPanel.Location = new System.Drawing.Point(6, 24);
+            this.ClientPanel.Name = "ClientPanel";
+            this.ClientPanel.Size = new System.Drawing.Size(490, 312);
+            this.ClientPanel.TabIndex = 2;
+            // 
+            // TranslatorSplitContainer
+            // 
+            this.TranslatorSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TranslatorSplitContainer.Location = new System.Drawing.Point(0, 24);
+            this.TranslatorSplitContainer.Name = "TranslatorSplitContainer";
+            // 
+            // TranslatorSplitContainer.Panel1
+            // 
+            this.TranslatorSplitContainer.Panel1.Controls.Add(this.SourceLanguageLabel);
+            this.TranslatorSplitContainer.Panel1.Controls.Add(this.SourceTextBox);
+            this.TranslatorSplitContainer.Panel1MinSize = 0;
+            // 
+            // TranslatorSplitContainer.Panel2
+            // 
+            this.TranslatorSplitContainer.Panel2.Controls.Add(this.TargetLanguageLabel);
+            this.TranslatorSplitContainer.Panel2.Controls.Add(this.TargetTextBox);
+            this.TranslatorSplitContainer.Panel2MinSize = 0;
+            this.TranslatorSplitContainer.Size = new System.Drawing.Size(502, 318);
+            this.TranslatorSplitContainer.SplitterDistance = 250;
+            this.TranslatorSplitContainer.SplitterWidth = 2;
+            this.TranslatorSplitContainer.TabIndex = 3;
+            this.TranslatorSplitContainer.Visible = false;
+            // 
+            // SourceLanguageLabel
+            // 
+            this.SourceLanguageLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.SourceLanguageLabel.AutoSize = true;
+            this.SourceLanguageLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.SourceLanguageLabel.Location = new System.Drawing.Point(3, 301);
+            this.SourceLanguageLabel.Name = "SourceLanguageLabel";
+            this.SourceLanguageLabel.Size = new System.Drawing.Size(44, 14);
+            this.SourceLanguageLabel.TabIndex = 2;
+            this.SourceLanguageLabel.Text = "English";
+            // 
+            // SourceTextBox
+            // 
+            this.SourceTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SourceTextBox.Font = new System.Drawing.Font("ＭＳ ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.SourceTextBox.Location = new System.Drawing.Point(0, 0);
+            this.SourceTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.SourceTextBox.MaxLength = 1000;
+            this.SourceTextBox.Multiline = true;
+            this.SourceTextBox.Name = "SourceTextBox";
+            this.SourceTextBox.Size = new System.Drawing.Size(250, 318);
+            this.SourceTextBox.TabIndex = 1;
+            // 
+            // TargetLanguageLabel
+            // 
+            this.TargetLanguageLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.TargetLanguageLabel.AutoSize = true;
+            this.TargetLanguageLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.TargetLanguageLabel.Location = new System.Drawing.Point(3, 301);
+            this.TargetLanguageLabel.Name = "TargetLanguageLabel";
+            this.TargetLanguageLabel.Size = new System.Drawing.Size(43, 14);
+            this.TargetLanguageLabel.TabIndex = 3;
+            this.TargetLanguageLabel.Text = "日本語";
+            // 
+            // TargetTextBox
+            // 
+            this.TargetTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TargetTextBox.Font = new System.Drawing.Font("ＭＳ ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.TargetTextBox.Location = new System.Drawing.Point(0, 0);
+            this.TargetTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.TargetTextBox.Multiline = true;
+            this.TargetTextBox.Name = "TargetTextBox";
+            this.TargetTextBox.ReadOnly = true;
+            this.TargetTextBox.Size = new System.Drawing.Size(250, 318);
+            this.TargetTextBox.TabIndex = 1;
+            // 
+            // StealthModeToolStripMenuItem
+            // 
+            this.StealthModeToolStripMenuItem.CheckOnClick = true;
+            this.StealthModeToolStripMenuItem.Name = "StealthModeToolStripMenuItem";
+            this.StealthModeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.StealthModeToolStripMenuItem.Text = "ステルスモード";
+            this.StealthModeToolStripMenuItem.Click += new System.EventHandler(this.StealthModeToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(502, 342);
+            this.Controls.Add(this.TranslatorSplitContainer);
+            this.Controls.Add(this.ClientPanel);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(200, 88);
+            this.Name = "MainForm";
+            this.Text = "Honyaku";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.SystemColors.ControlDark;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.TranslatorSplitContainer.Panel1.ResumeLayout(false);
+            this.TranslatorSplitContainer.Panel1.PerformLayout();
+            this.TranslatorSplitContainer.Panel2.ResumeLayout(false);
+            this.TranslatorSplitContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TranslatorSplitContainer)).EndInit();
+            this.TranslatorSplitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Panel ClientPanel;
+        private System.Windows.Forms.ToolStripMenuItem TranslateToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer TranslatorSplitContainer;
+        private System.Windows.Forms.TextBox SourceTextBox;
+        private System.Windows.Forms.TextBox TargetTextBox;
+        private System.Windows.Forms.ToolStripMenuItem BackToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ReTranslateToolStripMenuItem;
+        private System.Windows.Forms.Label SourceLanguageLabel;
+        private System.Windows.Forms.Label TargetLanguageLabel;
+        private System.Windows.Forms.ToolStripMenuItem 表示VToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 翻訳コンテナToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem LateralSplitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem VerticalitySplitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem StealthModeToolStripMenuItem;
     }
 }
 
--- honyaku/MainForm.cs
+++ honyaku/MainForm.cs
@@ -1,20 +1,189 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace honyaku
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        private AppMode AppMode = AppMode.Capture;
+
+        private bool StealthMode = false;
+
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.TargetTextBox.BackColor = Color.White;
+            this.DrawCaptureLine();
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            this.DrawCaptureLine();
+        }
+
+        private void DrawCaptureLine()
+        {
+            Bitmap canvas = new Bitmap(this.ClientPanel.Width, this.ClientPanel.Height);
+            Graphics g = Graphics.FromImage(canvas);
+            g.DrawRectangle(new Pen(Color.Red, 1), 0, 0, this.ClientPanel.Width - 1, this.ClientPanel.Height - 1);
+            g.Dispose();
+            this.ClientPanel.BackgroundImage = canvas;
+        }
+
+        private void ChangeMode(AppMode appMode)
+        {
+            this.AppMode = appMode;
+            if (appMode == AppMode.Capture)
+            {
+                this.TranslateToolStripMenuItem.Visible = true;
+                this.BackToolStripMenuItem.Visible = false;
+                this.ReTranslateToolStripMenuItem.Visible = false;
+                this.TranslatorSplitContainer.Visible = false;
+            }
+            else if(appMode == AppMode.Translate)
+            {
+                this.TranslateToolStripMenuItem.Visible = false;
+                this.BackToolStripMenuItem.Visible = true;
+                this.ReTranslateToolStripMenuItem.Visible = true;
+                this.TranslatorSplitContainer.Visible = true;
+
+                if(this.StealthMode)
+                {
+                    this.Opacity = 1;
+                }
+            }
+        }
+
+        private void BackToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.ChangeMode(AppMode.Capture);
+        }
+
+        private void TranslateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.TranslateToolStripMenuItem.Enabled = false;
+
+            Bitmap image = new Bitmap(this.ClientPanel.Width - 2, this.ClientPanel.Height - 2);
+            Graphics g = Graphics.FromImage(image);
+            Point ClientPanelPoint = this.ClientPanel.PointToScreen(new Point(0, 0));
+            g.CopyFromScreen(new Point(ClientPanelPoint.X + 1, ClientPanelPoint.Y + 1), new Point(0, 0), image.Size);
+            g.Dispose();
+
+            using (var tesseract = new Tesseract.TesseractEngine(@"tessdata", "eng"))
+            {
+                this.SourceTextBox.Text = tesseract.Process(image).GetText().Trim();
+                this.TargetTextBox.Text = "";
+
+                if (this.SourceTextBox.Text.Length == 0)
+                {
+                    MessageBox.Show("文字列が認識されませんでした。", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    goto end;
+                }
+
+                string str = Translator.Run(this.SourceTextBox.Text);
+                if (str == null)
+                {
+                    MessageBox.Show("翻訳時にエラーが発生しました。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    goto end;
+                }
+
+                this.TargetTextBox.Text = str;
+                this.ChangeMode(AppMode.Translate);
+            }
+
+            end:
+            this.TranslateToolStripMenuItem.Enabled = true;
+        }
+
+        private void ReTranslateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.ReTranslateToolStripMenuItem.Enabled = false;
+
+            this.SourceTextBox.Text = this.SourceTextBox.Text.Trim();
+            this.TargetTextBox.Text = "";
+
+            if (this.SourceTextBox.Text.Length == 0) goto end;
+
+            string str = Translator.Run(this.SourceTextBox.Text);
+            if (str == null)
+            {
+                MessageBox.Show("翻訳時にエラーが発生しました。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                goto end;
+            }
+
+            this.TargetTextBox.Text = str;
+
+            end:
+            this.ReTranslateToolStripMenuItem.Enabled = true;
+        }
+
+        private void LateralSplitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LateralSplitToolStripMenuItem.Checked = true;
+            this.VerticalitySplitToolStripMenuItem.Checked = false;
+            this.TranslatorSplitContainer.Orientation = Orientation.Vertical;
+            this.TranslatorSplitContainer.SplitterDistance = this.TranslatorSplitContainer.Width / 2;
+        }
+
+        private void VerticalitySplitToolStripMenuItem_Click(object sender, EventArgs e)
+        { 
+            this.LateralSplitToolStripMenuItem.Checked = false;
+            this.VerticalitySplitToolStripMenuItem.Checked = true;
+            this.TranslatorSplitContainer.Orientation = Orientation.Horizontal;
+            this.TranslatorSplitContainer.SplitterDistance = this.TranslatorSplitContainer.Height / 2;
+        }
+
+        private void menuStrip1_MouseEnter(object sender, EventArgs e)
+        {
+            if (this.AppMode == AppMode.Capture && StealthMode)
+            {
+                for (int i = 2; i <= 10; i++)
+                {
+                    this.Opacity = 0.1 * i;
+                    System.Threading.Thread.Sleep(10);
+                }
+            }
+        }
+
+        private void menuStrip1_MouseLeave(object sender, EventArgs e)
+        {
+            if (this.AppMode == AppMode.Capture && StealthMode)
+            {
+                for (int i = 10; i >= 3; i--)
+                {
+                    this.Opacity = 0.1 * i;
+                    System.Threading.Thread.Sleep(10);
+                }
+            }
+        }
+
+        private void StealthModeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.StealthMode = this.StealthModeToolStripMenuItem.Checked;
+            if (this.AppMode == AppMode.Capture)
+            {
+                if (this.StealthMode)
+                {
+                    for (int i = 10; i >= 3; i--)
+                    {
+                        this.Opacity = 0.1 * i;
+                        System.Threading.Thread.Sleep(10);
+                    }
+                }
+                else
+                {
+                    for (int i = 2; i <= 10; i++)
+                    {
+                        this.Opacity = 0.1 * i;
+                        System.Threading.Thread.Sleep(10);
+                    }
+                }
+            }
         }
     }
 }
