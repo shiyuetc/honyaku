1274954a9814226dc0c2f71ca13316bfcb3139b6
shiyuetc <shiyuetc@users.noreply.github.com>
2020-04-20 21:00:03 +0900

    Add translator

---
+++ honyaku/Translator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Script.Serialization;
+
+namespace honyaku
+{
+    public static class Translator
+    {
+        public static string Run(string input, string sourceLanguage = "en", string targetLanguage = "ja")
+        {
+            try
+            {
+                string url = String.Format("https://translate.googleapis.com/translate_a/single?client=gtx&sl={0}&tl={1}&dt=t&q={2}",
+                    sourceLanguage, targetLanguage, Uri.EscapeUriString(input));
+                HttpClient httpClient = new HttpClient();
+                string result = httpClient.GetStringAsync(url).Result;
+
+                var jsonData = new JavaScriptSerializer().Deserialize<List<dynamic>>(result);
+                var translationItems = jsonData[0];
+                string translation = "";
+
+                foreach (object item in translationItems)
+                {
+                    IEnumerable translationLineObject = item as IEnumerable;
+                    IEnumerator translationLineString = translationLineObject.GetEnumerator();
+                    translationLineString.MoveNext();
+                    translation += string.Format(" {0}", Convert.ToString(translationLineString.Current));
+                }
+
+                if (translation.Length > 1) { translation = translation.Substring(1); };
+                return translation;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
