7f39cfb9472de65ebe41e35c364bd5d7525e4827
shiyuetc <shiyuetc@users.noreply.github.com>
2020-04-21 12:09:20 +0900

    Add data property class

---
+++ honyaku/DataProperty.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections.Generic;
+
+namespace honyaku
+{
+    /// <summary>
+    /// グローバル変数
+    /// </summary>
+    public class DataProperty
+    {
+        public static List<Language> Languages = new List<Language>()
+        {
+            new Language("English", "英語", "en", "eng"),
+            new Language("Japanese", "日本語", "ja", "jpn"),
+        };
+    }
+}
