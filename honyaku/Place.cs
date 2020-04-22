7bcd441b84ee66839a21548db02ab60e18ac19b8
shiyuetc <shiyuetc@users.noreply.github.com>
2020-04-23 07:57:19 +0900

    Fix place class

--- honyaku/Place.cs
+++ honyaku/Place.cs
@@ -13,26 +13,26 @@ namespace honyaku
         public string Name { get; set; }
 
         /// <summary>
-        /// サイズ
-        /// </summary>
-        public Size Size { get; set; }
-
-        /// <summary>
         /// 位置
         /// </summary>
         public Point Location { get; set; }
 
         /// <summary>
+        /// サイズ
+        /// </summary>
+        public Size Size { get; set; }
+
+        /// <summary>
         /// コンストラクタ
         /// </summary>
         /// <param name="name">名前</param>
-        /// <param name="size">サイズ</param>
         /// <param name="location">位置</param>
-        public Place(string name, Size size, Point location)
+        /// <param name="size">サイズ</param>
+        public Place(string name, Point location, Size size)
         {
             this.Name = name;
-            this.Size = size;
             this.Location = location;
+            this.Size = size;
         }
     }
 }
