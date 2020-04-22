fe330d2e3d8eff69f3ea76984c968c330e5edc39
shiyuetc <shiyuetc@users.noreply.github.com>
2020-04-23 07:57:36 +0900

    Add windows api class

---
+++ honyaku/WindowsAPI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace honyaku
+{
+    /// <summary>
+    /// Windows API
+    /// </summary>
+    public class WindowsAPI
+    {
+        /// <summary>
+        /// マウス座標にあるハンドルを取得する
+        /// </summary>
+        /// <param name="point"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        public static extern IntPtr WindowFromPoint(Point point);
+
+        /// <summary>
+        /// 指定したハンドルからのアプリケーションにフォーカスを与える
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+    }
+}
