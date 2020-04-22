053715f192e50a28689d46faec91a1f14ba66531
shiyuetc <shiyuetc@users.noreply.github.com>
2020-04-23 07:59:02 +0900

    Add seeting form

---
+++ honyaku/SettingForm.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace honyaku
+{
+    partial class SettingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CaptureRegionMarginNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.CaptureRegionVisibleCheckBox = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.ApplyButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CaptureRegionMarginNumericUpDown)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(142, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 12);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "翻訳する言語：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "キャプチャする元の言語：";
+            // 
+            // CaptureRegionMarginNumericUpDown
+            // 
+            this.CaptureRegionMarginNumericUpDown.Location = new System.Drawing.Point(94, 79);
+            this.CaptureRegionMarginNumericUpDown.Maximum = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            this.CaptureRegionMarginNumericUpDown.Name = "CaptureRegionMarginNumericUpDown";
+            this.CaptureRegionMarginNumericUpDown.Size = new System.Drawing.Size(120, 19);
+            this.CaptureRegionMarginNumericUpDown.TabIndex = 3;
+            this.CaptureRegionMarginNumericUpDown.Value = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            // 
+            // CaptureRegionVisibleCheckBox
+            // 
+            this.CaptureRegionVisibleCheckBox.AutoSize = true;
+            this.CaptureRegionVisibleCheckBox.Checked = true;
+            this.CaptureRegionVisibleCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CaptureRegionVisibleCheckBox.Location = new System.Drawing.Point(17, 27);
+            this.CaptureRegionVisibleCheckBox.Name = "CaptureRegionVisibleCheckBox";
+            this.CaptureRegionVisibleCheckBox.Size = new System.Drawing.Size(94, 16);
+            this.CaptureRegionVisibleCheckBox.TabIndex = 2;
+            this.CaptureRegionVisibleCheckBox.Text = "領域線の表示";
+            this.CaptureRegionVisibleCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "領域線カラー：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "領域マージン：";
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.CloseButton.Location = new System.Drawing.Point(235, 273);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(100, 23);
+            this.CloseButton.TabIndex = 1;
+            this.CloseButton.Text = "キャンセル(&C)";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // ApplyButton
+            // 
+            this.ApplyButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ApplyButton.Location = new System.Drawing.Point(154, 273);
+            this.ApplyButton.Name = "ApplyButton";
+            this.ApplyButton.Size = new System.Drawing.Size(75, 23);
+            this.ApplyButton.TabIndex = 2;
+            this.ApplyButton.Text = "適用";
+            this.ApplyButton.UseVisualStyleBackColor = true;
+            this.ApplyButton.Click += new System.EventHandler(this.ApplyButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 65);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 12);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "ホットキー：";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(17, 42);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(144, 42);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 20);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(319, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "翻訳言語";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.CaptureRegionMarginNumericUpDown);
+            this.groupBox2.Controls.Add(this.CaptureRegionVisibleCheckBox);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 118);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(319, 149);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "キャプチャ領域";
+            // 
+            // SettingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 308);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ApplyButton);
+            this.Controls.Add(this.CloseButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SettingForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "設定";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.SettingForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CaptureRegionMarginNumericUpDown)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Button ApplyButton;
+        private System.Windows.Forms.NumericUpDown CaptureRegionMarginNumericUpDown;
+        private System.Windows.Forms.CheckBox CaptureRegionVisibleCheckBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
---
+++ honyaku/SettingForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace honyaku
+{
+    public partial class SettingForm : Form
+    {
+
+
+        public SettingForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SettingForm_Load(object sender, EventArgs e)
+        {
+            this.CaptureRegionMarginNumericUpDown.Value = (int)Properties.Settings.Default["capture_region_margin"];
+            this.CaptureRegionVisibleCheckBox.Checked = (bool)Properties.Settings.Default["capture_region_visible"];
+            //this.CaptureRegionColor = (Color)Properties.Settings.Default["capture_region_color"];
+        }
+
+        private void ApplyButton_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
