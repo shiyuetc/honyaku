8bc30614f8dc002effd64f6832792af3752a73f5
shiyuetc <shiyuetc@users.noreply.github.com>
2020-04-22 12:36:02 +0900

    Add quick capture form

---
+++ honyaku/QuickCaptureForm.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace honyaku
+{
+    partial class QuickCaptureForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // QuickCaptureForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(100, 100);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "QuickCaptureForm";
+            this.Opacity = 0.3D;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "QuickCaptureForm";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.QuickCaptureForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.QuickCaptureForm_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.QuickCaptureForm_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ honyaku/QuickCaptureForm.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace honyaku
+{
+    /// <summary>
+    /// 簡易キャプチャーフォーム
+    /// </summary>
+    public partial class QuickCaptureForm : Form
+    {
+        /// <summary>
+        /// 指定した範囲をキャプチャしたイメージ
+        /// </summary>
+        public Bitmap ResultImage = null;
+
+        /// <summary>
+        /// マウスダウン座標、マウスアップ座標
+        /// </summary>
+        private Point MouseDownPoint;
+
+        /// <summary>
+        /// マウスダウンフラグ
+        /// </summary>
+        private bool MouseDownFlag;
+
+        /// <summary>
+        /// 枠線のペンを定義
+        /// </summary>
+        private Pen RedPen = new Pen(Color.Red, 3f);
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public QuickCaptureForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// フォームのマウスダウンイベント
+        /// </summary>
+        private void QuickCaptureForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right) this.Close();
+            this.MouseDownPoint = Cursor.Position;
+            this.MouseDownFlag = true;
+        }
+
+        /// <summary>
+        /// フォームのマウスアップイベント
+        /// </summary>
+        private void QuickCaptureForm_MouseUp(object sender, MouseEventArgs e)
+        {
+            // 描画を消去
+            this.Refresh();
+
+            Point mousePoint = Cursor.Position;
+
+            Size pictureSize = new Size(
+                Math.Abs(this.MouseDownPoint.X - mousePoint.X),
+                Math.Abs(this.MouseDownPoint.Y - mousePoint.Y)
+            );
+
+            if (this.MouseDownFlag && pictureSize.Width > 0 && pictureSize.Height > 0)
+            {
+                this.ResultImage = new Bitmap(pictureSize.Width, pictureSize.Height);
+                Graphics g = Graphics.FromImage(this.ResultImage);
+                g.CopyFromScreen(new Point(
+                    this.MouseDownPoint.X - mousePoint.X > 0 ? mousePoint.X : this.MouseDownPoint.X,
+                    this.MouseDownPoint.Y - mousePoint.Y > 0 ? mousePoint.Y : this.MouseDownPoint.Y
+                ), new Point(0, 0), this.ResultImage.Size);
+                g.Dispose();
+            }
+            
+            this.Close();
+        }
+
+        /// <summary>
+        /// フォームのマウス移動イベント
+        /// </summary>
+        private void QuickCaptureForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(this.MouseDownFlag)
+            {
+                // 描画を消去
+                this.Refresh();
+
+                Point mousePoint = Cursor.Position;
+                Graphics g = CreateGraphics();
+                g.DrawLine(RedPen, this.MouseDownPoint.X, this.MouseDownPoint.Y, mousePoint.X, this.MouseDownPoint.Y);
+			    g.DrawLine(RedPen, this.MouseDownPoint.X, this.MouseDownPoint.Y, this.MouseDownPoint.X, mousePoint.Y);
+		        g.DrawLine(RedPen, mousePoint.X, this.MouseDownPoint.Y, mousePoint.X, mousePoint.Y);
+			    g.DrawLine(RedPen, this.MouseDownPoint.X, mousePoint.Y, mousePoint.X, mousePoint.Y);
+                g.Dispose();
+            }
+        }
+    }
+}
