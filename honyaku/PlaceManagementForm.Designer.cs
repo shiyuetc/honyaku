3ab686046068b3101b03b246fa1d28b24a8b73a3
shiyuetc <shiyuetc@users.noreply.github.com>
2020-04-23 07:58:12 +0900

    Add place management form

---
+++ honyaku/PlaceManagementForm.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace honyaku
+{
+    partial class PlaceManagementForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PlaceListDataGridView = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.配置項目ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AddPlaceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.DeletePlaceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.NameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LocationColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SizeColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LocationXColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LocationYColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SizeWidthColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SizeHeightColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.PlaceListDataGridView)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PlaceListDataGridView
+            // 
+            this.PlaceListDataGridView.AllowUserToAddRows = false;
+            this.PlaceListDataGridView.AllowUserToDeleteRows = false;
+            this.PlaceListDataGridView.AllowUserToResizeRows = false;
+            this.PlaceListDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PlaceListDataGridView.BackgroundColor = System.Drawing.Color.White;
+            this.PlaceListDataGridView.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.PlaceListDataGridView.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.PlaceListDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.PlaceListDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NameColumn,
+            this.LocationColumn,
+            this.SizeColumn,
+            this.LocationXColumn,
+            this.LocationYColumn,
+            this.SizeWidthColumn,
+            this.SizeHeightColumn});
+            this.PlaceListDataGridView.Location = new System.Drawing.Point(12, 38);
+            this.PlaceListDataGridView.MultiSelect = false;
+            this.PlaceListDataGridView.Name = "PlaceListDataGridView";
+            this.PlaceListDataGridView.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.PlaceListDataGridView.RowHeadersVisible = false;
+            this.PlaceListDataGridView.RowTemplate.Height = 21;
+            this.PlaceListDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.PlaceListDataGridView.Size = new System.Drawing.Size(380, 291);
+            this.PlaceListDataGridView.TabIndex = 2;
+            this.PlaceListDataGridView.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.PlaceListDataGridView_CellDoubleClick);
+            this.PlaceListDataGridView.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.PlaceListDataGridView_CellValidating);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.配置項目ToolStripMenuItem,
+            this.SetToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(404, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 配置項目ToolStripMenuItem
+            // 
+            this.配置項目ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AddPlaceToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.DeletePlaceToolStripMenuItem});
+            this.配置項目ToolStripMenuItem.Name = "配置項目ToolStripMenuItem";
+            this.配置項目ToolStripMenuItem.Size = new System.Drawing.Size(91, 20);
+            this.配置項目ToolStripMenuItem.Text = "項目の編集(&E)";
+            // 
+            // AddPlaceToolStripMenuItem
+            // 
+            this.AddPlaceToolStripMenuItem.Name = "AddPlaceToolStripMenuItem";
+            this.AddPlaceToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.AddPlaceToolStripMenuItem.Text = "現在の配置を記憶";
+            this.AddPlaceToolStripMenuItem.Click += new System.EventHandler(this.AddPlaceToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // DeletePlaceToolStripMenuItem
+            // 
+            this.DeletePlaceToolStripMenuItem.Name = "DeletePlaceToolStripMenuItem";
+            this.DeletePlaceToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.DeletePlaceToolStripMenuItem.Text = "選択項目を削除";
+            this.DeletePlaceToolStripMenuItem.Click += new System.EventHandler(this.DeletePlaceToolStripMenuItem_Click);
+            // 
+            // SetToolStripMenuItem
+            // 
+            this.SetToolStripMenuItem.Name = "SetToolStripMenuItem";
+            this.SetToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.SetToolStripMenuItem.Text = "配置";
+            this.SetToolStripMenuItem.Click += new System.EventHandler(this.SetToolStripMenuItem_Click);
+            // 
+            // NameColumn
+            // 
+            this.NameColumn.HeaderText = "名前";
+            this.NameColumn.MinimumWidth = 100;
+            this.NameColumn.Name = "NameColumn";
+            // 
+            // LocationColumn
+            // 
+            this.LocationColumn.HeaderText = "座標位置";
+            this.LocationColumn.Name = "LocationColumn";
+            this.LocationColumn.ReadOnly = true;
+            this.LocationColumn.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // SizeColumn
+            // 
+            this.SizeColumn.HeaderText = "ウィンドウ幅";
+            this.SizeColumn.Name = "SizeColumn";
+            this.SizeColumn.ReadOnly = true;
+            this.SizeColumn.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.SizeColumn.Width = 150;
+            // 
+            // LocationXColumn
+            // 
+            this.LocationXColumn.HeaderText = "Location-X";
+            this.LocationXColumn.Name = "LocationXColumn";
+            this.LocationXColumn.Visible = false;
+            // 
+            // LocationYColumn
+            // 
+            this.LocationYColumn.HeaderText = "Location-Y";
+            this.LocationYColumn.Name = "LocationYColumn";
+            this.LocationYColumn.Visible = false;
+            // 
+            // SizeWidthColumn
+            // 
+            this.SizeWidthColumn.HeaderText = "Size-Width";
+            this.SizeWidthColumn.Name = "SizeWidthColumn";
+            this.SizeWidthColumn.Visible = false;
+            // 
+            // SizeHeightColumn
+            // 
+            this.SizeHeightColumn.HeaderText = "Size-Height";
+            this.SizeHeightColumn.Name = "SizeHeightColumn";
+            this.SizeHeightColumn.Visible = false;
+            // 
+            // PlaceManagementForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 341);
+            this.Controls.Add(this.PlaceListDataGridView);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "PlaceManagementForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "ウィンドウ配置管理";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PlaceManagementForm_FormClosing);
+            this.Load += new System.EventHandler(this.PlaceManagementForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PlaceListDataGridView)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView PlaceListDataGridView;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 配置項目ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AddPlaceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem DeletePlaceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SetToolStripMenuItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NameColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LocationColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SizeColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LocationXColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LocationYColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SizeWidthColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SizeHeightColumn;
+    }
+}
---
+++ honyaku/PlaceManagementForm.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace honyaku
+{
+    /// <summary>
+    /// ウィンドウ配置管理フォーム
+    /// </summary>
+    public partial class PlaceManagementForm : Form
+    {
+        /// <summary>
+        /// メインのフォーム
+        /// </summary>
+        private MainForm MainForm;
+
+        /// <summary>
+        /// 配置データのファイルパス
+        /// </summary>
+        private readonly string FileName = "Places.xml";
+
+        /// <summary>
+        /// 編集フラグ
+        /// </summary>
+        private bool Edit = false;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="mainForm">メインのフォーム</param>
+        public PlaceManagementForm(MainForm mainForm)
+        {
+            InitializeComponent();
+            this.MainForm = mainForm;
+        }
+
+        /// <summary>
+        /// フォームのロードイベント
+        /// </summary>
+        private void PlaceManagementForm_Load(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists(this.FileName))
+            {
+                try
+                {
+                    System.Xml.Serialization.XmlSerializer serializer =
+                        new System.Xml.Serialization.XmlSerializer(typeof(Place[]));
+                    System.IO.StreamReader sr = new System.IO.StreamReader(FileName, new System.Text.UTF8Encoding(false));
+                    Place[] places = (Place[])serializer.Deserialize(sr);
+                    sr.Close();
+
+                    DataGridViewRow[] rows = new DataGridViewRow[places.Length];
+                    for (int i = 0; i < places.Length; i++)
+                    {
+                        DataGridViewRow row = new DataGridViewRow();
+                        row.CreateCells(this.PlaceListDataGridView);
+                        row.SetValues(places[i].Name, places[i].Location.ToString(), places[i].Size.ToString(),
+                            places[i].Location.X, places[i].Location.Y, places[i].Size.Width, places[i].Size.Height);
+                        rows[i] = row;
+                    }
+                    this.PlaceListDataGridView.Rows.AddRange(rows);
+                }
+                catch
+                {
+                    MessageBox.Show("配置データの読み込みに失敗しました。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        /// <summary>
+        /// フォームの終了イベント
+        /// </summary>
+        private void PlaceManagementForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.Edit)
+            {
+                try
+                {
+                    Place[] places = new Place[this.PlaceListDataGridView.Rows.Count];
+                    for (int i = 0; i < this.PlaceListDataGridView.Rows.Count; i++)
+                    {
+                        DataGridViewRow row = this.PlaceListDataGridView.Rows[i];
+                        places[i] = new Place(row.Cells["NameColumn"].Value.ToString(),
+                            new Point((int)row.Cells["LocationXColumn"].Value, (int)row.Cells["LocationYColumn"].Value),
+                            new Size((int)row.Cells["SizeWidthColumn"].Value, (int)row.Cells["SizeHeightColumn"].Value));
+                    }
+
+                    System.Xml.Serialization.XmlSerializer serializer =
+                        new System.Xml.Serialization.XmlSerializer(typeof(Place[]));
+                    System.IO.StreamWriter sw = new System.IO.StreamWriter(FileName, false, new System.Text.UTF8Encoding(false));
+                    serializer.Serialize(sw, places);
+                    sw.Close();
+                }
+                catch
+                {
+                    MessageBox.Show("配置データの書き込みに失敗しました。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 現在の配置を記憶ボタンのクリックイベント
+        /// </summary>
+        private void AddPlaceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.PlaceListDataGridView.Rows.Add("配置" + (this.PlaceListDataGridView.Rows.Count + 1), this.MainForm.Location.ToString(), this.MainForm.Size.ToString(),
+            this.MainForm.Location.X, this.MainForm.Location.Y, this.MainForm.Size.Width, this.MainForm.Size.Height);
+            this.Edit = true;
+        }
+
+        /// <summary>
+        /// 選択項目を削除ボタンのクリックイベント
+        /// </summary>
+        private void DeletePlaceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.PlaceListDataGridView.SelectedRows.Count == 1)
+            {
+                this.PlaceListDataGridView.Rows.Remove(this.PlaceListDataGridView.SelectedRows[0]);
+                this.Edit = true;
+            }
+        }
+
+        /// <summary>
+        /// 配置ボタンのクリックイベント
+        /// </summary>
+        private void SetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.PlaceListDataGridView.SelectedRows.Count == 1)
+            {
+                var row = this.PlaceListDataGridView.SelectedRows[0];
+                this.MainForm.Location = new Point((int)row.Cells["LocationXColumn"].Value, (int)row.Cells["LocationYColumn"].Value);
+                this.MainForm.Size = new Size((int)row.Cells["SizeWidthColumn"].Value, (int)row.Cells["SizeHeightColumn"].Value);
+            }
+        }
+
+        /// <summary>
+        /// セルの値変更イベント
+        /// </summary>
+        private void PlaceListDataGridView_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            this.Edit = true;
+        }
+
+        /// <summary>
+        /// セルのダブルクリックイベント
+        /// </summary>
+        private void PlaceListDataGridView_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex > -1)
+            {
+                var row = this.PlaceListDataGridView.Rows[e.RowIndex];
+                this.MainForm.Location = new Point((int)row.Cells["LocationXColumn"].Value, (int)row.Cells["LocationYColumn"].Value);
+                this.MainForm.Size = new Size((int)row.Cells["SizeWidthColumn"].Value, (int)row.Cells["SizeHeightColumn"].Value);
+            }
+        }
+    }
+}
