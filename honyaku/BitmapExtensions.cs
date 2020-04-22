1a1cd4a5433dc223216f628abb3e77622d8a9f09
shiyuetc <shiyuetc@users.noreply.github.com>
2020-04-22 12:32:57 +0900

    Add to_ocr_string method

---
+++ honyaku/BitmapExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace honyaku
+{
+    /// <summary>
+    /// Bitmapクラスの拡張メソッド
+    /// </summary>
+    public static class BitmapExtensions
+    {
+        /// <summary>
+        /// 光学文字認識による画像から文字列の読み出し
+        /// </summary>
+        /// <param name="bmp">元の画像</param>
+        /// <param name="targetLanguageISO639_2">抽出する言語コード</param>
+        /// <returns>抽出した文字列</returns>
+        public static string ToOcrString(this Bitmap bmp, string targetLanguageISO639_2 = "eng")
+        {
+            using (var tesseract = new Tesseract.TesseractEngine(DataProperty.TessDataFolder, targetLanguageISO639_2))
+            {
+                return tesseract.Process(bmp).GetText().Trim();
+            }
+        }
+    }
+}
